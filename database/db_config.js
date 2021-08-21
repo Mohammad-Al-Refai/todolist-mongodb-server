bf5f29073030ce900b9f6bd7a78ce6fc9aff1ded
Mohammad-AL-Rifai <55941955+Mohammad-Al-Refai@users.noreply.github.com>
2021-08-21 16:34:41 +0300

    Update db_config.js

--- database/db_config.js
+++ database/db_config.js
@@ -1,6 +1,6 @@
 const DB_NAME="data"
 const TABLE="values"
-const MONGODB_URL=`mongodb+srv://root:VaJiUP7dzpEAqKGO@cluster0.ukkim.mongodb.net/data?authSource=admin&replicaSet=atlas-ou4utr-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`
+const MONGODB_URL=`URL`//MongoDB server url
 
 
 
